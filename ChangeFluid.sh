ca9321e09f113c3e76d8945e87e8cd5050610e50
DarkAce65 <tvdood99@gmail.com>
2016-02-24 22:10:24 -0500

    Make shell script edit behavioral values via pipe

--- ChangeFluid.sh
+++ ChangeFluid.sh
@@ -1,11 +1,44 @@
 #!/bin/bash
-DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-NEW_FLUID=$DIR"/share/SARndbox-1.6/Shaders/Fluids/SurfaceAddWaterColor-"$1".fs"
-SHADER=$DIR"/share/SARndbox-1.6/Shaders/SurfaceAddWaterColor.fs"
-if [ -e $shader ] ; then
-	cp $NEW_FLUID $SHADER
-else
-	echo "Fluid shader not found."
-	echo "Valid fluid options are:"
-	echo "Water, Lava, ToxicWaste, Grayscale"
+DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)" # Get shell directory
+DEST_SHADER=$DIR"/share/SARndbox-1.6/Shaders/SurfaceAddWaterColor.fs" # Destination shader path
+
+PIPE_PATH=$DIR"/Control.fifo";
+if [ ! -e $PIPE_PATH ] # Ensure that an empty pipe will be available
+	then
+		echo "Control.fifo not found."
+		echo "Run 'mkfifo Control.fifo' and pass it to SARndbox via -cp flag"
+		echo "./SARndbox <usual parameters> -cp "$DIR"/Control.fifo"
+		exit 1
 fi
+
+shopt -u nocasematch # Case insensitive patterns
+case $1 in
+	water | w)
+		SOURCE_SHADER=$DIR"/share/SARndbox-1.6/Shaders/Fluids/SurfaceAddWaterColor-Water.fs"
+		echo "waterSpeed 4.0" > $PIPE_PATH
+		echo "waterAttenuation 0.0078125" > $PIPE_PATH # 1/128
+		;;
+	lava | l)
+		SOURCE_SHADER=$DIR"/share/SARndbox-1.6/Shaders/Fluids/SurfaceAddWaterColor-Lava.fs"
+		echo "waterSpeed 1.0" > $PIPE_PATH
+		echo "waterAttenuation 0.99" > $PIPE_PATH
+		;;
+	toxic waste | toxicwaste | tw)
+		SOURCE_SHADER=$DIR"/share/SARndbox-1.6/Shaders/Fluids/SurfaceAddWaterColor-Water.fs"
+		echo "waterSpeed 1.5" > $PIPE_PATH
+		echo "waterAttenuation 0.3" > $PIPE_PATH
+		;;
+	grayscale | g)
+		SOURCE_SHADER=$DIR"/share/SARndbox-1.6/Shaders/Fluids/SurfaceAddWaterColor-Water.fs"
+		echo "waterSpeed 2.0" > $PIPE_PATH
+		echo "waterAttenuation 0.05" > $PIPE_PATH
+		;;
+	*)
+		echo "Fluid shader not found."
+		echo "Available fluids are: Water, Lava, Toxic Waste, Grayscale"
+		echo "Fluids may also be passed by first initial e.g. Water as w, Toxic Waste as tw"
+		exit 1
+		;;
+esac
+
+cp $SOURCE_SHADER $DEST_SHADER # Copy new fluid to active shader
