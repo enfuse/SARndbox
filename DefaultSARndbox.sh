80de21dcc55cdf79d1b069a7aec823fab09d1d34
DarkAce65 <tvdood99@gmail.com>
2016-02-25 10:03:09 -0500

    Create DefaultSARndbox script

---
+++ DefaultSARndbox.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)" # Get shell directory
+
+# Ensure that an empty pipe will be available
+$PIPE_PATH=$DIR"/Control.fifo"
+if [ -e $PIPE_PATH ]
+	then
+		rm $PIPE_PATH
+fi
+mkfifo $PIPE_PATH
+
+# Initialize with water
+SHADER_PATH=$DIR"share/SARndbox-1.6/Shaders"
+cp $SHADER_PATH"/Fluids/SurfaceAddWaterColor-Water.fs" $SHADER_PATH"/SurfaceAddWaterColor.fs"
+
+# Keep a terminal open with SARndbox running
+xfce4-terminal --hold --working-directory $DIR"/bin" --command "./SARndbox -fpv -ws 4.0 30 -rs 1.0"
